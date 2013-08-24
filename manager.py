9d204dbf017e51bedb0b3caa7627f8610f6aaae1
Zespre Schmidt <starbops@gmail.com>
2013-08-24 18:40:20 +0800

    Imporve coding style according to PEP-8.
    - Indentation (soft tab)
    - Maximum 79 characters in one line
    - One-line docstrings
    - In 'hashfile', using `while buf:` instead of `while len(buf) > 0:`
    
    Caution! Not fully tested!

--- manager.py
+++ manager.py
@@ -1,181 +1,187 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 # file: manager.py
 
-import dbus, getopt, hashlib, os, pycassa, re, subprocess, sys
+import dbus
+import getopt
+import hashlib
+import os
+import re
+import subprocess as subp
+import sys
+
+import pycassa
 from pycassa.pool import ConnectionPool
 from pycassa.columnfamily import ColumnFamily
 
-SECMAPSERVLST = ['192.168.100.103', '192.168.100.105', '192.168.100.107', '192.168.100.111']
+SECMAP_SERVLST = ['192.168.100.103', '192.168.100.105',
+                 '192.168.100.107', '192.168.100.111'
+]
 
-###
-# Return A Dictionary About UUID & Mount Point
-####################################################################################
 def uuidpth():
-	bus = dbus.SystemBus()
-	ud_manager_obj = bus.get_object("org.freedesktop.UDisks", "/org/freedesktop/UDisks")
-	ud_manager = dbus.Interface(ud_manager_obj, 'org.freedesktop.UDisks')
-	uuidlst = []
-	mntpthlst = []
+    """Return a dictionary about UUID & mount point."""
+    bus = dbus.SystemBus()
+    ud_manager_obj = bus.get_object("org.freedesktop.UDisks",
+                                    "/org/freedesktop/UDisks")
+    ud_manager = dbus.Interface(ud_manager_obj,
+                                'org.freedesktop.UDisks')
+    uuidlst = []
+    mntpthlst = []
 
-	for dev in ud_manager.EnumerateDevices():
-		device_obj = bus.get_object("org.freedesktop.UDisks", dev)
-		device_props = dbus.Interface(device_obj, dbus.PROPERTIES_IFACE)
-		mntpth = str(device_props.Get('org.freedesktop.UDisks.Device', "DeviceMountPaths"))
-		iduuid = str(device_props.Get('org.freedesktop.UDisks.Device', "IdUuid"))
-		try: mntpthlst.append(re.search('.*\'(/.*?)\'.*', mntpth).group(1))
-		except AttributeError: continue
-		if iduuid != '': uuidlst.append(iduuid)
+    for dev in ud_manager.EnumerateDevices():
+        device_obj = bus.get_object("org.freedesktop.UDisks", dev)
+        device_props = dbus.Interface(device_obj, dbus.PROPERTIES_IFACE)
+        mntpth = str(device_props.Get('org.freedesktop.UDisks.Device',
+                                      "DeviceMountPaths"))
+        iduuid = str(device_props.Get('org.freedesktop.UDisks.Device',
+                                      "IdUuid"))
+        try:
+            mntpthlst.append(re.search('.*\'(/.*?)\'.*', mntpth).group(1))
+        except AttributeError:
+            continue
 
-	return dict(zip(uuidlst, mntpthlst))
+        if iduuid != '':
+            uuidlst.append(iduuid)
+    return dict(zip(uuidlst, mntpthlst))
 
-###
-# Insert Key With Column Name And Column Value
-####################################################################################
 def InsertKey(handle, key, col_val_list):
-	col_val_list = iter(col_val_list)
-	for col, val in zip(col_val_list, col_val_list):
-		handle.insert(key, {col: val})
-		print "[INFO] Key '" + key + "' inserted."
+    """Insert key with column name and column value."""
+    col_val_list = iter(col_val_list)
+    for col, val in zip(col_val_list, col_val_list):
+        handle.insert(key, {col: val})
+        print "[INFO] Key '" + key + "' inserted."
 
-###
-# MD5 & SHA1
-####################################################################################
 def hashfile(file_name, hasherlst, file_size, blocksize=65536):
-	print "[INFO] Hashing file '" + file_name + "'."
-	if file_size == 0:
-		[hasher.update('') for hasher in hasherlst]
-	else:
-		with open(file_name, 'rb') as afile:
-			buf = afile.read(blocksize)
-			while len(buf) > 0:
-				[hasher.update(buf) for hasher in hasherlst]
-				buf = afile.read(blocksize)
-	return ''.join([hasher.hexdigest() for hasher in hasherlst]) + str(file_size)
+    """Generate a string containing md5, sha1, and file size."""
+    print "[INFO] Hashing file '" + file_name + "'."
+    if file_size == 0:
+        [hasher.update('') for hasher in hasherlst]
+    else:
+        with open(file_name, 'rb') as afile:
+            buf = afile.read(blocksize)
+            while buf:
+                [hasher.update(buf) for hasher in hasherlst]
+                buf = afile.read(blocksize)
 
-###
-# Check If Current Directory Is In Blacklist
-####################################################################################
+    hashstr = ''.join([hasher.hexdigest() for hasher in hasherlst])
+    result = hashstr + str(file_size)
+    return result
+
 def inblacklist(nm, blklst):
-	for blk in blklst:
-		if nm.startswith(blk):
-			return True
-	return False
+    """Check if current directory is in blacklist."""
+    for blk in blklst:
+        if nm.startswith(blk):
+            return True
+    return False
 
-###
-# Insert Keys From A Given Point Recursively (UUID)
-####################################################################################
 def ImportTree(h, devuuid, pth, blklst=[]):
-	for drnm, sbdrnms, flnms in os.walk(pth):
-		print "[INFO] Entering '" + drnm + "'."
-		flnmabslst = [os.path.abspath(os.path.join(drnm, flnm)) for flnm in flnms]
-		for flnmabs in flnmabslst:
-			if inblacklist(flnmabs, blklst): continue
-			else:
-				try:
-					hashstr = hashfile(flnmabs, [hashlib.md5(), hashlib.sha1()], os.path.getsize(flnmabs))
-					InsertKey(h, flnmabs, [devuuid, hashstr])
-				except Exception as err:
-					print "[ERROR] " + str(err) + "."
-	print "[INFO] Importation done."
+    """Insert keys from a given point recursively (UUID)."""
+    for drnm, sbdrnms, flnms in os.walk(pth):
+        print "[INFO] Entering '" + drnm + "'."
+        flnmabslst = [os.path.abspath(os.path.join(drnm, flnm))
+                          for flnm in flnms
+        ]
+        for flnmabs in flnmabslst:
+            if inblacklist(flnmabs, blklst):
+                continue
+            else:
+                try:
+                    hashstr = hashfile(flnmabs,
+                                       [hashlib.md5(), hashlib.sha1()],
+                                       os.path.getsize(flnmabs))
+                    InsertKey(h, flnmabs, [devuuid, hashstr])
+                except Exception as err:
+                    print "[ERROR] " + str(err) + "."
+    print "[INFO] Importation done."
 
-###
-# Delete Whole Row Or Multiple Columns
-####################################################################################
 def DeleteKey(handle, key, col):
-	if len(col) != 0:
-		handle.remove(key, col);
-		print "[INFO] Remove " + str(col) + " from '" + key + "'."
-	else:
-		handle.remove(key);
-		print "[INFO] Remove '" + key + "'."
+    """Delete whole row or multiple columns."""
+    if len(col) != 0:
+        handle.remove(key, col);
+        print "[INFO] Remove " + str(col) + " from '" + key + "'."
+    else:
+        handle.remove(key);
+        print "[INFO] Remove '" + key + "'."
 
-###
-# Get File Content From Other Cassandra Servers
-####################################################################################
 def getfilecontent(taskid):
-	result = ""
-	try:
-		(p, c) = connectserver(SECMAPSERVLST, 'SECMAP', 'SUMMARY')
-	except Exception as err:
-		print "[ERROR] " + str(err) + "\n[ERROR] Connection aborted."
-	else:
-		try:
-			content = GetKey(c, taskid, ['content'], False)
-			result = content.values()[0]
-		except pycassa.NotFoundException as err:
-			print "[ERROR] " + str(err) + "."
-		p.dispose()
-	return result
+    """Get file content from other cassandra servers."""
+    result = ""
+    try:
+        (p, c) = connectserver(SECMAP_SERVLST, 'SECMAP', 'SUMMARY')
+    except Exception as err:
+        print "[ERROR] " + str(err) + "\n[ERROR] Connection aborted."
+    else:
+        try:
+            content = GetKey(c, taskid, ['content'], False)
+            result = content.values()[0]
+        except pycassa.NotFoundException as err:
+            print "[ERROR] " + str(err) + "."
+        p.dispose()
+    return result
 
-###
-# Write Content Into File
-####################################################################################
 def setfile(file_name, content):
-	try: os.makedirs(os.getcwd() + os.path.dirname(file_name))
-	except OSError as err:
-		print "[ERROR] " + str(err) + "."
-	full_file_name = os.getcwd() + file_name
-	with open(full_file_name, "w") as f:
-		f.write(content)
-	if re.match(r".*DLL.*", subprocess.check_output(['file', full_file_name])):
-		new_file_name = full_file_name + ".dll"
-	else:
-		new_file_name = full_file_name + ".exe"
-	os.system("mv " + full_file_name + " " + new_file_name)
+    """Write content into file."""
+    try:
+        os.makedirs(os.getcwd() + os.path.dirname(file_name))
+    except OSError as err:
+        print "[ERROR] " + str(err) + "."
+    full_file_name = os.getcwd() + file_name
+    with open(full_file_name, "w") as f:
+        f.write(content)
 
-###
-# Return OrderedDict Of The Specified Key (Along With Column Name)
-####################################################################################
+    if re.match(r".*DLL.*",
+            subp.check_output(['file', full_file_name])):
+        new_file_name = full_file_name + ".dll"
+    else:
+        new_file_name = full_file_name + ".exe"
+    os.system("mv " + full_file_name + " " + new_file_name)
+
 def GetKey(handle, key, col, download=True):
-	result = handle.get(key, col)
-	if download:
-		for content in result.values():
-			setfile(key, getfilecontent(content))
-	return result
+    """Return OrderedDict of the specified key (along with column name)."""
+    result = handle.get(key, col)
+    if download:
+        for content in result.values():
+            setfile(key, getfilecontent(content))
+    return result
 
-###
-# Show The Number Of Total Rows / Show Column Names Of The Specified Key
-####################################################################################
 def ListInfo(handle, key_list):
-	if len(key_list) == 0:
-		result = dict(handle.get_range(column_count=0, filter_empty=False))
-		print "Total", len(result), "row(s)."
-		#return result
-	else:
-		result_col = GetKey(handle, key_list[0], '').keys()
-		print "'" + key_list[0] + "' has " + str(result_col) + " column(s)."
-		return result_col
+    """Show the number of total rows/show column names of the specified key."""
+    if len(key_list) == 0:
+        result = dict(handle.get_range(column_count=0,
+                      filter_empty=False))
+        print "Total", len(result), "row(s)."
+        #return result
+    else:
+        result_col = GetKey(handle, key_list[0], '').keys()
+        print "'" + key_list[0] + "' has " + str(result_col) + " column(s)."
+        return result_col
 
-###
-# Columns Given, Return Keys
-####################################################################################
 def ComplexGet(handle, key_list, col_list):
-	if len(key_list) == 0:
-		result = dict(handle.get_range(columns=col_list, buffer_size=1024)).keys()
-		for x in result: print x
-	else:
-		result = dict(handle.multiget(key_list, col_list, buffer_size=1024))
-		for x in result.values(): print x.items()[0][1]
-	return result
+    """Return keys given columns."""
+    if len(key_list) == 0:
+        result = dict(handle.get_range(columns=col_list,
+                      buffer_size=1024)).keys()
+        for x in result:
+            print x
+    else:
+        result = dict(handle.multiget(key_list, col_list,
+                      buffer_size=1024))
+        for x in result.values():
+            print x.items()[0][1]
+    return result
 
-###
-# Establish Connection
-####################################################################################
 def connectserver(address, key_space, column_family):
-	pool = ConnectionPool(key_space, address)
-	print "[INFO] Connection to '" + key_space + "' established."
-	cf = ColumnFamily(pool, column_family)
-	print "[INFO] Column family '" + column_family + "' used."
-	return pool, cf
+    """Establish connection."""
+    pool = ConnectionPool(key_space, address)
+    print "[INFO] Connection to '" + key_space + "' established."
+    cf = ColumnFamily(pool, column_family)
+    print "[INFO] Column family '" + column_family + "' used."
+    return pool, cf
 
-###
-# Show Help Messages
-####################################################################################
 def helpmsg():
-	print "Usage: ./manager.py -h server -k keyspace -c columnfamily -i key column value"
-	print "       ./manager.py -h server -k keyspace -c columnfamily -I [path]"
-	print "       ./manager.py -h server -k keyspace -c columnfamily -d key [column]"
-	print "       ./manager.py -h server -k keyspace -c columnfamily -l key column"
-	print "       ./manager.py -h server -k keyspace -c columnfamily -L [key]"
-	print "       ./manager.py -h server -k keyspace -c columnfamily -M [key_string] column_string"
+    """Show help messages."""
+    print "Usage: ./manager.py -h serv -k keyspace -c col_fam -i key col val"
+    print "       ./manager.py -h serv -k keyspace -c col_fam -I [path]"
+    print "       ./manager.py -h serv -k keyspace -c col_fam -d key [col]"
+    print "       ./manager.py -h serv -k keyspace -c col_fam -l key col"
+    print "       ./manager.py -h serv -k keyspace -c col_fam -L [key]"
+    print "       ./manager.py -h serv -k keyspace -c col_fam -M [key_str] col_str"
--- setup.py
+++ setup.py
@@ -1,72 +1,75 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 # file: setup.py
 
-import getopt, pycassa, sys
+import getopt
+import sys
+
+import pycassa
 from pycassa.system_manager import *
 
 def GetHandle(address):
-	handle = SystemManager(address)
-	print "[INFO] Connection established."
-	return handle
+    handle = SystemManager(address)
+    print "[INFO] Connection established."
+    return handle
 
 def BuildDB(handle, key_space, col_fam):
-	handle.create_keyspace(key_space, SIMPLE_STRATEGY, {'replication_factor': '1'})
-	print "[INFO] Keyspace '%s' was created." % key_space
-	handle.create_column_family(key_space, col_fam)
-	print "[INFO] Column Family '%s' was created." % col_fam
+    handle.create_keyspace(key_space, SIMPLE_STRATEGY,
+                           {'replication_factor': '1'})
+    print "[INFO] Keyspace '%s' was created." % key_space
+    handle.create_column_family(key_space, col_fam)
+    print "[INFO] Column Family '%s' was created." % col_fam
 
 def DestroyDB(handle, key_space):
-	handle.drop_keyspace(key_space)
-	print "[INFO] Keyspace '%s'was dropped." % key_space
+    handle.drop_keyspace(key_space)
+    print "[INFO] Keyspace '%s'was dropped." % key_space
 
 def HelpMsg():
-	print "Usage: ./setup.py -h server -C keyspace columnfamily"
-	print "       ./setup.py -h server -D keyspace"
+    print "Usage: ./setup.py -h server -C keyspace col_fam"
+    print "       ./setup.py -h server -D keyspace"
 
 def main():
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], "h:C:D:")
-		if len(opts) != 2:
-			raise getopt.GetoptError, "Need at least one option."
-	except getopt.GetoptError:
-		HelpMsg()
-	else:
-		in_server = opts[0][1]
-		(o, a) = opts[1]
-		if o == '-C':
-			if a != '' and len(args) == 1:
-				try:
-					sm = GetHandle(in_server)
-				except:
-					print "[ERROR] Cannot connect to '%s'." % str(a)
-					sys.exit(2)
-				try:
-					BuildDB(sm, a, args[0])
-				except pycassa.InvalidRequestException as err:
-					print "[ERROR] " + str(err) + "."
-					print "[ERROR] Failed to create keyspace or column family."
-				sm.close()
-				print "[INFO] Connection closed."
-			else:
-				HelpMsg()
-		elif o == '-D':
-			if a != '':
-				try:
-					sm = GetHandle(in_server)
-				except:
-					print "[ERROR] Cannot connect to '%s'." % str(a)
-					sys.exit(2)
-				try:
-					DestroyDB(sm, a)
-				except pycassa.InvalidRequestException:
-					print "[ERROR] Keyspace '%s' not found." % a
-				sm.close()
-				print "[INFO] Connection closed."
-			else:
-				HelpMsg()
-		else:
-			HelpMsg()
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "h:C:D:")
+        if len(opts) != 2:
+            raise getopt.GetoptError, "Need at least one option."
+    except getopt.GetoptError:
+        HelpMsg()
+    else:
+        in_server = opts[0][1]
+        (o, a) = opts[1]
+        if o == '-C':
+            if a != '' and len(args) == 1:
+                try:
+                    sm = GetHandle(in_server)
+                except:
+                    print "[ERROR] Cannot connect to '%s'." % str(a)
+                    sys.exit(2)
+                try:
+                    BuildDB(sm, a, args[0])
+                except pycassa.InvalidRequestException as err:
+                    print "[ERROR] " + str(err) + "."
+                    print "[ERROR] Failed to create keyspace or column family."
+                sm.close()
+                print "[INFO] Connection closed."
+            else:
+                HelpMsg()
+        elif o == '-D':
+            if a != '':
+                try:
+                    sm = GetHandle(in_server)
+                except:
+                    print "[ERROR] Cannot connect to '%s'." % str(a)
+                    sys.exit(2)
+                try:
+                    DestroyDB(sm, a)
+                except pycassa.InvalidRequestException:
+                    print "[ERROR] Keyspace '%s' not found." % a
+                sm.close()
+                print "[INFO] Connection closed."
+            else:
+                HelpMsg()
+        else:
+            HelpMsg()
 
 if __name__ == '__main__':
-	main()
+    main()
