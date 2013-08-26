5643624ac6c056c5faba4da8ed765714df5e28e5
Zespre Schmidt <starbops@gmail.com>
2013-08-26 21:49:56 +0800

    Functions
    get_values
    set_file

--- get_file.py
+++ get_file.py
@@ -8,11 +8,45 @@ import pycassa
 from pycassa.pool import ConnectionPool
 from pycassa.columnfamily import ColumnFamily
 
-SERVLIST = ['192.168.100.103', '192.168.100.105',
+SERVERLIST = ['192.168.100.103', '192.168.100.105',
             '192.168.100.107', '192.168.100.111'
 ]
 KEYSPACE = 'SECMAP'
 COLUMNFAMILY = 'SUMMARY'
+
+def get_values(servlst, ks, cf, key):
+    try:
+        pool = ConnectionPool(ks, servlst)
+        cf_handle = ColumnFamily(pool, cf)
+        result = cf_handle.get(key).values()
+    except pycassa.NotFoundException as err:
+        print "[ERROR] " + key + " not found"
+        exit(-1)
+    except Exception as err:
+        print "[ERROR] " + str(err)
+        exit(-1)
+    finally:
+        pool.dispose()
+
+    return result
+
+def set_file(filename, file_content):
+    full_filename = os.getcwd() + filename
+    full_filepath = os.path.dirname(full_filename)
+
+    try:
+        os.makedirs(full_filepath)
+    except OSError as err:
+        print "[WARN] " + str(err)
+
+    try:
+        with open(full_filename, 'wb') as file_handle:
+            file_handle.write(file_content)
+    except IOError as err:
+        print "[ERROR] " + str(err)
+        exit(-1)
+
+    return "%s" % full_filename
 
 def usage():
     return "Usage:\npython %s <serv> <keyspace> <col_fam> <key>" % sys.argv[0]
@@ -27,50 +61,15 @@ def main():
         print usage()
         exit(-1)
 
-    try:
-        frontend = ConnectionPool(keyspace, [serverlist])
-        frontend_cf = ColumnFamily(frontend, columnfamily)
-    except Exception as err:
-        print "[ERROR] " + str(err)
-        exit(-1)
-    print "connection to frontend established"
-    try:
-        taskid = frontend_cf.get(target_filename).values()
-    except pycassa.NotFoundException as err:
-        print "[ERROR] " + str(err)
-        exit(-1)
-    finally:
-        frontend.dispose()
-        print "connection closed"
-    print "taskid: " + str(taskid)
+    taskid_list = get_values(serverlist, keyspace,
+                             columnfamily, target_filename)
+    for taskid in taskid_list:
+        print taskid
+        content_list = get_values(SERVERLIST, KEYSPACE, COLUMNFAMILY, taskid)
 
-    try:
-        backend = ConnectionPool(KEYSPACE, SERVLIST)
-        backend_cf = ColumnFamily(backend, COLUMNFAMILY)
-    except Exception as err:
-        print "[ERROR] " + str(err)
-        exit(-1)
-    print "connection to backend established"
-    try:
-        content = backend_cf.get(taskid[0], ['content']).values()[0]
-    except pycassa.NotFoundException as err:
-        print "[ERROR] " + str(err)
-        exit(-1)
-    finally:
-        backend.dispose()
-        print "connection closed"
-    print "file size: " + str(len(content)) + " bytes"
-
-    full_filename = os.getcwd() + target_filename
-    full_filepath = os.path.dirname(full_filename)
-    try:
-        os.makedirs(full_filepath)
-    except OSError as err:
-        print "[WARN] " + str(err)
-    with open(full_filename, 'wb') as file_handle:
-        file_handle.write(content)
-    print "file location: " + full_filename
+    content = content_list[0]
+    file_location = set_file(target_filename, content)
+    print file_location
 
 if __name__ == '__main__':
     main()
-
