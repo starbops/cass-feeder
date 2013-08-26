449cdfc701249c0e6c4ff6215aebbb7bf9c472cf
Zespre Schmidt <starbops@gmail.com>
2013-08-26 12:26:38 +0800

    For your own safety.

--- get_file.py
+++ get_file.py
@@ -8,27 +8,27 @@ import pycassa
 from pycassa.pool import ConnectionPool
 from pycassa.columnfamily import ColumnFamily
 
-SERVLIST_1 = ['140.113.121.170']
-KEYSPACE_1 = 'ks'
-COLUMNFAMILY_1 = 'cf'
-SERVLIST_2 = ['192.168.100.103', '192.168.100.105',
+SERVLIST = ['192.168.100.103', '192.168.100.105',
               '192.168.100.107', '192.168.100.111'
 ]
-KEYSPACE_2 = 'SECMAP'
-COLUMNFAMILY_2 = 'SUMMARY'
+KEYSPACE = 'SECMAP'
+COLUMNFAMILY = 'SUMMARY'
 
 def main():
-    target_filename = sys.argv[1]
+    serverlist = sys.argv[1]
+    keyspace = sys.argv[2]
+    columnfamily = sys.argv[3]
+    target_filename = sys.argv[4]
 
-    frontend = ConnectionPool(KEYSPACE_1, SERVLIST_1)
-    frontend_cf = ColumnFamily(frontend, COLUMNFAMILY_1)
+    frontend = ConnectionPool(keyspace, serverlist)
+    frontend_cf = ColumnFamily(frontend, columnfamily)
     print "connection to frontend established"
     taskid = frontend_cf.get(target_filename).values()
     print "taskid: " + str(taskid)
     frontent.dispose()
 
-    backend = ConnectionPool(KEYSPACE_2, SERVLIST_2)
-    backend_cf = ColumnFamily(backend, COLUMNFAMILY_2)
+    backend = ConnectionPool(KEYSPACE, SERVLIST)
+    backend_cf = ColumnFamily(backend, COLUMNFAMILY)
     print "connection to backend established"
     content = backend_cf.get(taskid, ['content']).values()
     print "file size: " + str(len(content)) + " bytes"
