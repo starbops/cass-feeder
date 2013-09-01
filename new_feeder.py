9f44cf3b569691bc6ffeba220d0292acc60848fd
Zespre Schmidt <starbops@gmail.com>
2013-09-02 01:29:30 +0800

    以 device_uuid 為單位建立 cassandra 的連線
    不再是以前每插入一筆資料就建立一次連線
    試圖修復未知的連線 error

--- new_feeder.py
+++ new_feeder.py
@@ -12,7 +12,7 @@ from pycassa.pool import ConnectionPool
 from pycassa.columnfamily import ColumnFamily
 
 PREFIX = "/mnt"
-F_SERVLIST = ["140.113.121.170"]
+F_SERVLIST = ["localhost"]
 F_KS = "ks"
 F_CF = "cf"
 B_SERVLIST = ["192.168.100.103", "192.168.100.105",
@@ -61,29 +61,53 @@ def gen_taskid(hasherlist, filename, file_content):
     hash_str = ''.join([hasher.hexdigest() for hasher in hasherlist])
     return hash_str + str(os.path.getsize(filename))
 
-def feeder(filename, device_uuid, taskid, file_content):
-    frontend = {}
-    backend = {}
-    try:
-        frontend['pool'] = ConnectionPool(F_KS, F_SERVLIST)
-        frontend['handle'] = ColumnFamily(frontend['pool'], F_CF)
-        backend['pool'] = ConnectionPool(B_KS, B_SERVLIST)
-        backend['handle'] = ColumnFamily(backend['pool'], B_CF)
-    except pycassa.NotFoundException as err:
-        print "[ERROR] " + str(err)
-        exit(-1)
-    except pycassa.AllServersUnavailable as err:
-        print "[ERROR] " + str(err)
-        exit(-1)
+#def feeder(filename, device_uuid, taskid, file_content):
+#    frontend = {}
+#    backend = {}
+#    try:
+#        frontend['pool'] = ConnectionPool(F_KS, F_SERVLIST)
+#        frontend['handle'] = ColumnFamily(frontend['pool'], F_CF)
+#        backend['pool'] = ConnectionPool(B_KS, B_SERVLIST)
+#        backend['handle'] = ColumnFamily(backend['pool'], B_CF)
+#    except pycassa.NotFoundException as err:
+#        print "[ERROR] " + str(err)
+#        exit(-1)
+#    except pycassa.AllServersUnavailable as err:
+#        print "[ERROR] " + str(err)
+#        exit(-1)
+#
+#    print filename
+#    frontend['handle'].insert(filename, {device_uuid: taskid})
+#    print taskid
+#    backend['handle'].insert(taskid, {'content': file_content})
+#    print "Uploaded\n"
+#
+#    frontend['pool'].dispose()
+#    backend['pool'].dispose()
 
-    print filename
-    frontend['handle'].insert(filename, {device_uuid: taskid})
-    print taskid
-    backend['handle'].insert(taskid, {'content': file_content})
-    print "Uploaded\n"
+def new_feeder(device_uuid, filelist):
+    with ConnectionPool(F_KS, F_SERVLIST) as f_pool:
+        f_handle = ColumnFamily(f_pool, F_CF)
+        with ConnectionPool(B_KS, B_SERVLIST) as b_pool:
+            b_handle = ColumnFamily(b_pool, B_CF)
 
-    frontend['pool'].dispose()
-    backend['pool'].dispose()
+            counter = 0
+            for filename in filelist:
+                file_content = ""
+                with open(filename, "rb") as f:
+                    file_content = f.read(BLOCKSIZE)
+
+                hasherlist = [hashlib.md5(), hashlib.sha1()]
+                taskid = gen_taskid(hasherlist, filename, file_content)
+                print sp
+                print filename
+                f_handle.insert(filename, {device_uuid: taskid})
+                print taskid
+                b_handle.insert(taskid, {'content': file_content})
+                print "Uploaded\n"
+                counter += 1
+
+                print "Total " + str(counter) + " file(s) uploaded."
 
 def main():
     try:
@@ -101,19 +125,7 @@ def main():
             print "[WARN] No files were found"
             exit(-1)
 
-        counter = 0
-        for filename in filelist:
-            file_content = ""
-            with open(filename, "rb") as f:
-                file_content = f.read(BLOCKSIZE)
-
-            hasherlist = [hashlib.md5(), hashlib.sha1()]
-            taskid = gen_taskid(hasherlist, filename, file_content)
-            print sp
-            feeder(filename, sp, taskid, file_content)
-            counter += 1
-
-        print "Total " + str(counter) + " file(s) uploaded."
+        new_feeder(sp, filelist)
 
 if __name__ == "__main__":
     main()
